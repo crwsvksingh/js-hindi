e556fe40698b7b1063bda97753802aabce91d010
Virendra Singh <v.ksingh@rediffmail.com>
2024-06-22 06:49:12 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//All java codes treat in newer version of java script
+//alert("Virendra")// alert will not work in the node JS it only work in browesers
+console.log(3+3);
+console.log("virendra");//code readability should be high
+//use mdn or https://tc39.es/ecma262/ for documentation pupose
+
+/*Datatypes are - 
+    string => ""
+    number => 2 to the power 53
+    bigint
+    boolean => true/false
+    null => objects
+    undefined
+    objects
+    symbole=> unique
+*/
+
+console.log(typeof(null));
