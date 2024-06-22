db4fd615f0fd27f445632792add544fb859b6d21
Virendra Singh <v.ksingh@rediffmail.com>
2024-06-22 06:33:38 +0000

    variables

--- 01_basics/variables.js
+++
@@ -1,8 +0,0 @@
-const accountId = 12456
-let accountEmail = "virendrasingh@gmaul.com"
-let accountPassword = "123456789"
-/*
-Prefer to use let and const to declare variables
-*/
-console.log(accountId);
-console.table([accountId,accountEmail,accountPassword]);
