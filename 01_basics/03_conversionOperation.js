7d503a8dcadbff33ac551c71013cbebc8b9d7fa1
Virendra Singh <v.ksingh@rediffmail.com>
2024-06-22 07:07:08 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = null;
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+/*
+for conversion of string into Number
+"33" => 33
+"33abc" => Nan
+true => 1
+false => 0
+for conversion of string into Boolean
+1=> true, 0=> false
+""=> false
+"virendra" => true
+*/
